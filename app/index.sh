30305a9a97a8b633500d31383a5081865b2b7a10
Hamza-Shafee-Aldaghstany <118183676+Hamza-Shafee-Aldaghstany@users.noreply.github.com>
2025-04-20 22:44:13 +0300

    Update index.sh

--- app/index.sh
+++ app/index.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 echo "This script include commands to run mapreduce jobs using hadoop streaming to index documents"
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
+    -input /data \
+    -output /tmp/index1 \
+    -mapper "python3 mapper1.py" \
+    -reducer "python3 reducer1.py" \
+    -file mapreduce/mapper1.py \
+    -file mapreduce/reducer1.py
 
 echo "Input file is :"
 echo $1
